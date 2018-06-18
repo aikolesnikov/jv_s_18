7e2f2d060eefac141831bcff8764aa36ebc88c55
Artyom Kolesnikov <artyom.kolesnikov@gmail.com>
2018-06-18 16:39:40 +0300

    mvn structure

---
+++ src/main/java/lesson_01/Main.java
@@ -0,0 +1,7 @@
+package lesson_01;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Yes");
+    }
+}
---
+++ src/test/java/lesson_01/MyTests.java
@@ -0,0 +1,14 @@
+package lesson_01;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class MyTests {
+
+    @Test
+    public void firstOne() {
+        assertEquals(0, 0);
+    }
+
+}
