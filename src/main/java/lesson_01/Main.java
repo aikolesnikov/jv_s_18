f5cbbbd136941852e9efde769e1ffccd5bdbf634
Artyom Kolesnikov <artyom.kolesnikov@gmail.com>
2018-06-18 18:43:39 +0300

    printTwo

--- src/main/java/lesson_01/Main.java
+++ src/main/java/lesson_01/Main.java
@@ -19,7 +19,7 @@ public class Main {
         for (int i = 0; i < size; i++) {
 
             for (int j = 1; j <= size; j++) {
-                if (j<=i)
+                if (j <= i)
                     System.out.print(' ');
                 else
                     System.out.print('*');
