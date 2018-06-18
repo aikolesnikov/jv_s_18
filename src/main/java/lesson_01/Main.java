866a34dc21a08c1bbf2f729411c9cc0f8fad546b
Artyom Kolesnikov <artyom.kolesnikov@gmail.com>
2018-06-18 18:43:08 +0300

    printTwo

--- src/main/java/lesson_01/Main.java
+++ src/main/java/lesson_01/Main.java
@@ -3,9 +3,9 @@ package lesson_01;
 public class Main {
 
     private final static int size = 11;
-    private static int k = 0;
 
     private static void printOne() {
+        int k;
         for (int i = 0; i < size; i++) {
             k = (i < size / 2) ? i : size - i - 1;
             for (int j = 0; j <= k; j++) {
@@ -15,11 +15,23 @@ public class Main {
         }
     }
 
+    private static void printTwo() {
+        for (int i = 0; i < size; i++) {
 
+            for (int j = 1; j <= size; j++) {
+                if (j<=i)
+                    System.out.print(' ');
+                else
+                    System.out.print('*');
+            }
+            System.out.println();
+        }
+    }
 
     public static void main(String[] args) {
 
-        printOne();
+        // printOne();
+        printTwo();
 
     }
 }
