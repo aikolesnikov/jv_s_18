495caad4f0410e1ef6b71ec8ccee82c86bfb3ae5
Artyom Kolesnikov <artyom.kolesnikov@gmail.com>
2018-06-18 18:17:38 +0300

    printOne

--- src/main/java/lesson_01/Main.java
+++ src/main/java/lesson_01/Main.java
@@ -1,7 +1,25 @@
 package lesson_01;
 
 public class Main {
+
+    private final static int size = 11;
+    private static int k = 0;
+
+    private static void printOne() {
+        for (int i = 0; i < size; i++) {
+            k = (i < size / 2) ? i : size - i - 1;
+            for (int j = 0; j <= k; j++) {
+                System.out.print('*');
+            }
+            System.out.println();
+        }
+    }
+
+
+
     public static void main(String[] args) {
-        System.out.println("Yes");
+
+        printOne();
+
     }
 }
