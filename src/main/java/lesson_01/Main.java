deb853e06c6dd239e0a29e7ba76cfd749fd5247e
Artyom Kolesnikov <artyom.kolesnikov@gmail.com>
2018-06-19 12:12:40 +0300

    printCorners

--- src/main/java/lesson_01/Main.java
+++ src/main/java/lesson_01/Main.java
@@ -4,7 +4,8 @@ public class Main {
 
     private final static int size = 11;
 
-    private static void printOne() {
+
+    private static void printRightTopRightBottomByOneCycle() {
         int k;
         for (int i = 0; i < size; i++) {
             k = (i < size / 2) ? i : size - i - 1;
@@ -15,11 +16,22 @@ public class Main {
         }
     }
 
-    private static void printTwo() {
+    private static void printLeftTopLeftBottomByOneCycle() {
+        int k;
         for (int i = 0; i < size; i++) {
+            k = (i < size / 2) ? i : size - i - 1;
+            for (int j = 0; j <= k; j++) {
+                System.out.print('*');
+            }
+            System.out.println();
+        }
+    }
 
+
+    private static void printLeftTop() {
+        for (int i = size; i > 0; i--) {
             for (int j = 1; j <= size; j++) {
-                if (j <= i)
+                if (j < i)
                     System.out.print(' ');
                 else
                     System.out.print('*');
@@ -28,7 +40,7 @@ public class Main {
         }
     }
 
-    private static void printThree() {
+    private static void printLeftBottom() {
         for (int i = 0; i < size; i++) {
 
             for (int j = 0; j < size; j++) {
@@ -41,11 +53,35 @@ public class Main {
         }
     }
 
+    private static void printRightTop() {
+        for (int i = 0; i < size; i++) {
+
+            for (int j = 1; j <= size; j++) {
+                if (j > i)
+                    System.out.print(' ');
+                else
+                    System.out.print('*');
+            }
+            System.out.println();
+        }
+    }
+
+    private static void printRightBottom() {
+        for (int i = 0; i < size; i++) {
+
+            for (int j = size; j >= 0; j--) {
+                if (j > i)
+                    System.out.print('*');
+                else
+                    System.out.print(' ');
+            }
+            System.out.println();
+        }
+    }
+
     public static void main(String[] args) {
 
-        // printOne();
-        // printTwo();
-        printThree();
+        // ? printLeftTopLeftBottomByOneCycle();
 
     }
 }
