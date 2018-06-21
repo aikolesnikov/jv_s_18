c193d87b12fa11f083a7811e5f0992bbd067eb77
Artyom Kolesnikov <artyom.kolesnikov@gmail.com>
2018-06-21 17:50:57 +0300

    Triangle

---
+++ src/main/java/lesson_02/model/Triangle.java
@@ -0,0 +1,23 @@
+package lesson_02.model;
+
+public class Triangle {
+
+    private double radius;
+
+    public Triangle() {
+    }
+
+    public Triangle(double radius){
+        this.radius = radius;
+    }
+
+    public double getRadius() {
+        return radius;
+    }
+
+    public void setRadius( double radius ) {
+        this.radius = radius;
+    }
+
+
+}
