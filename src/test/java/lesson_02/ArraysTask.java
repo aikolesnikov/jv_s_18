c704a62cf874f8cf70e57c0a0ad63f9cfc121e30
Artyom Kolesnikov <artyom.kolesnikov@gmail.com>
2018-06-21 13:00:13 +0300

    arrays_2

--- src/test/java/lesson_02/ArraysTask.java
+++ src/test/java/lesson_02/ArraysTask.java
@@ -39,9 +39,88 @@ public class ArraysTask {
         return ar2;
     }
 
-    public static void main( String[] args ) {
-        System.out.println( Arrays.toString( Task14( new int[]{ 1, 2, 3, 14, 5, 6 } ) ) );
+    private static void Task15(int[] ar) {
+        int negCount = 0;
+        int posCount = 0;
 
+        for ( int anAr : ar ) {
+            if ( ( anAr ) < 0 ) {
+                negCount++;
+            }
+            if ( ( anAr ) > 0 ) {
+                posCount++;
+            }
+        }
+
+        int[] arNeg = new int[negCount];
+        int[] arPos = new int[posCount];
+
+        int jNeg = 0, jPos = 0;
+
+        for (int i = 0; i < ar.length; i++) {
+            if ((ar[i]) < 0) {
+                arNeg[jNeg++] = ar[i];
+            }
+            if ((ar[i]) > 0) {
+                posCount++;
+                arPos[jPos++] = ar[i];
+            }
+        }
+
+        System.out.println(Arrays.toString(arNeg));
+        System.out.println(Arrays.toString(arPos));
     }
 
+    private static int[] Task16(int[] ar1, int[] ar2) {
+
+        int c = 0;
+        for (int i = 0; i < ar1.length; i++) {
+            if ((ar2[i] % 2 == 0) && (ar2[i] % 2 == 0)) {
+                c++;
+            }
+        }
+
+        int[] ar3 = new int[c * 2];
+
+        c = 0;
+        for (int i = 0; i < ar1.length; i++) {
+            if (!((ar2[i] % 2 == 0) && (ar2[i] % 2 == 0))) {
+                ar3[c++] = ar1[i];
+                ar3[c++] = ar2[i];
+            }
+        }
+        return ar3;
+    }
+
+    private static int[] Task17(int[] ar) {
+        int min = ar[0];
+        int max = ar[0];
+
+        for (int i = 1; i < ar.length; i++) {
+            if (ar[i] < min) {
+                min = ar[i];
+            } else if (ar[i] > max) {
+                max = ar[i];
+            }
+        }
+
+
+        int c = 0;
+        int[] tmpAr = new int[ar.length - 1];
+        for (int i = 0; i < ar.length; i++) {
+            if (!((ar[i] == min) || (ar[i] == max))) {
+                tmpAr[c++] = ar[i];
+            }
+        }
+
+        return Arrays.copyOf(tmpAr, c);
+    }
+
+    public static void main(String[] args) {
+        // System.out.println(Arrays.toString(Task14(new int[]{1, 2, 3, 4, 5, 6})));
+        // Task15(new int[]{1, 2, 3, 4, -5, 6});
+        // System.out.println(Arrays.toString(Task16(new int[]{1, 2, 3, 4, 5, 6}, new int[]{3, 4, 5, 6, 7, 8})));
+
+        System.out.println(Arrays.toString(Task17(new int[]{8, 2, 3, 7, 8})));
+    }
 }
