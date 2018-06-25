afc1539f13d8cdb81d2e3a736cb263db21cb92f2
Artyom Kolesnikov <artyom.kolesnikov@gmail.com>
2018-06-25 17:40:48 +0300

    Comparator

--- src/main/java/lesson_02/controller/Main.java
+++ src/main/java/lesson_02/controller/Main.java
@@ -12,13 +12,8 @@ public class Main {
     public static void main( String[] args ) {
 
         Rectangle r1 = new Rectangle( 20, 10, "red" );
-        // System.out.println( r1);
-
         Circle c1 = new Circle( "grey", 5 );
-        // System.out.println(c1);
-
         Triangle t1 = new Triangle("blue", 10, 12, 15);
-        // System.out.println(t1);
 
         Shape[] sAr = new Shape[10];
         sAr[0] = r1;
