9a875bb064cc92e8412bdb3b06c6a57abd734ba5
Artyom Kolesnikov <artyom.kolesnikov@gmail.com>
2018-06-30 15:12:22 +0300

    HomeTask_02_Book_01

---
+++ books/src/test/java/TestMain.java
@@ -0,0 +1,15 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Created by kaim on 6/30/18.
+ */
+public class TestMain {
+
+    @Test
+    public void firstOne() {
+        assertEquals(0, 0);
+    }
+
+    }
