7fc5ed7fe2adba8962abcb0502e8e50c96d6fb14
Artyom Kolesnikov <artyom.kolesnikov@gmail.com>
2018-07-04 13:26:07 +0300

    ToDo

---
+++ src/main/java/lesson_04/homework4/Main.java
@@ -0,0 +1,18 @@
+package lesson_04.homework4;
+
+/**
+ * Created by kaim on 6/25/18.
+ */
+public class Main {
+
+    public static void main(String[] args) {
+
+        String s = "abc d";
+
+        System.out.println(s.length());
+        System.out.println(s.trim().length());
+    }
+
+
+
+}
