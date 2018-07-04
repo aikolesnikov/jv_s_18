1993d72fe3c1b67e4579d247cc0ace3f374d089d
Artyom Kolesnikov <artyom.kolesnikov@gmail.com>
2018-07-04 17:27:22 +0300

    String1

--- src/main/java/lesson_04/homework4/Main.java
+++ src/main/java/lesson_04/homework4/Main.java
@@ -3,26 +3,50 @@ package lesson_04.homework4;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.Arrays;
-import java.util.Scanner;
+import java.util.*;
 
 /**
  * Created by kaim on 6/25/18.
  */
 public class Main {
 
-    private static String rawText;
-    private static String linkToBook = "/home/kaim/prog/text.txt";
+    private static String bookText;
+    private static String bookFile = "/home/kaim/prog/text.txt";
+
+    public static void main( String[] args ) {
+        Main program = new Main();
+        program.setBookText();
+
+        ArrayList<String> sentences = new ArrayList<>( 10 );
+
+        Scanner sc = new Scanner( bookText );
+        sc.useDelimiter( "[!?.]" );
+        String temp_sentence;
+        while ( sc.hasNext() ) {
+            temp_sentence = sc.next();
+            if ( !(temp_sentence.isEmpty()) ) {
+                sentences.add( temp_sentence.trim() );
+            }
+        }
+        sc.close();
+
+        sentences.trimToSize();
+        System.out.println( sentences.size() );
+
+        for ( String s : sentences ) {
+            System.out.println( s );
+        }
+    }
 
     private void setBookText() {
         try {
-            rawText = readFile(linkToBook);
-        } catch (IOException e) {
+            bookText = readFile( bookFile );
+        } catch ( IOException e ) {
             e.printStackTrace();
         }
     }
 
-    private String readFile(String fileName) throws IOException {
+    private String readFile( String fileName ) throws IOException {
 
         try ( BufferedReader bufferedReader = new BufferedReader( new FileReader( fileName ) ) ) {
             StringBuilder stringBuilder = new StringBuilder();
@@ -37,27 +61,4 @@ public class Main {
         }
 
     }
-
-    public static void main(String[] args) {
-        Main program = new Main();
-        program.setBookText();
-
-        System.out.println("-- raw -----------------------------");
-        System.out.println(rawText);
-
-        System.out.println("-- scanner -----------------------------");
-        Scanner sc = new Scanner( rawText );
-        sc.useDelimiter( "[!?.]" );
-        while (sc.hasNext()){
-            System.out.println(sc.next().trim());
-        }
-        sc.close();
-
-        System.out.println("-- split -----------------------------");
-        for ( String s : rawText.split( "[!?.]" )) {
-            System.out.println(s.trim());
-        }
-
-    }
-
 }
