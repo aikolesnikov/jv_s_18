fe0c1643a961e040eb406f01de963bfd71162cac
Artyom Kolesnikov <artyom.kolesnikov@gmail.com>
2018-07-05 18:17:01 +0300

    Objects

--- src/main/java/lesson_04/homework4/BreakIterTest.java
+++ src/main/java/lesson_04/homework4/BreakIterTest.java
@@ -3,18 +3,18 @@ package lesson_04.homework4;
 import lesson_04.homework4.utils.SystemUtils;
 
 import java.text.BreakIterator;
-import java.util.Locale;
 
 import static lesson_04.homework4.view.TextPrinter.printEachForward;
 
 public class BreakIterTest {
 
-
-
     public static void main( String[] args ) throws Exception{
         // String stringToExamine = "Uhu. Lalal? Ogo, ogoo!! Yes... No no no.";
 
         String stringToExamine = SystemUtils.readFile( "/home/kaim/prog/text.txt" );
+
+        System.out.println(stringToExamine);
+        System.out.println("-------------------------------");
 
         BreakIterator boundary = BreakIterator.getLineInstance();
         boundary.setText(stringToExamine);
